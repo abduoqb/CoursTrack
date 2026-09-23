6ec2628abe593c614596068ac0823528f682c11e
Abdul <pikatchu838@gmail.com>
2026-09-23 23:45:25 +0200

    style: inverse les icones importer et exporter

--- js/subject.js
+++ js/subject.js
@@ -103,7 +103,7 @@ function renderItem(categoryName, item) {
                     </button>
                 ` : ''}
                 <label class="btn-icon" title="Importer un fichier" style="cursor:pointer;">
-                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="17 8 12 3 7 8"/><line x1="12" y1="3" x2="12" y2="15"/></svg>
+                    <svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"/><polyline points="7 10 12 15 17 10"/><line x1="12" y1="15" x2="12" y2="3"/></svg>
                     <input type="file" class="import-file-input"
                         data-category="${escapeHtml(categoryName)}"
                         data-item-id="${item.id}"
